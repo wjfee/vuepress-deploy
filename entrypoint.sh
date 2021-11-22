8624b0f679fb7dd4316cc351a3418942e37581d9
wjfee <93143933+wjfee@users.noreply.github.com>
2021-11-22 21:10:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,8 +21,8 @@ do
   echo "start build in : `pwd`"
   eval "$BUILD_SCRIPT"
   echo "${i} Build success"
-  echo `pwd`
-  echo `ls ${GITHUB_WORKSPACE}`
+  echo "after build now in : `pwd`"
+  echo `ls ${dir}`
 done
 
 
